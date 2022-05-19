30d1e116fb097b106fc39b2ccf7e93b194023f79
jeveryun <jeveryun@qq.com>
2022-05-19 15:52:25 +0800

    check args

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
-// now is 03.4-6
-
-module.exports = core;
+// now is 03.4-8
 
 // require .js/.json/.node
+
+const path = require('path');
+
 const semver = require('semver');
 const colors = require('colors/safe');
 const userHome = require('user-home');
@@ -14,6 +15,10 @@ const log = require('@j-cli-dev/log');
 
 const pkg = require('../package.json');
 const constant = require('./const');
+let args;
+let config;
+
+module.exports = core;
 
 function core() {
   try {
@@ -21,13 +26,53 @@ function core() {
     checkPkgVersion();
     checkRoot();
     checkUserHome();
+    checkInputArgs();
+    checkEnv();
   } catch (error) {
     log.error(error.message);
   }
 }
 
+function checkEnv() {
+  const dotenv = require('dotenv');
+  const dotenvPath = path.resolve(userHome, '.env');
+  if (pathExists(dotenvPath)) {
+    dotenv.config({
+      path: dotenvPath,
+    });
+  }
+  config = createDefaultConfig();
+  log.verbose('环境变量', process.env.CLI_HOME_PATH);
+}
+
+function createDefaultConfig() {
+  const cliConfig = {
+    home: userHome,
+  };
+  if (process.env.CLI_HOME) {
+    cliConfig['cliHome'] = path.join(userHome, process.env.CLI_HOME);
+  } else {
+    cliConfig['cliHome'] = path.join(userHome, constant.DEFAULT_CLI_HOME);
+  }
+  process.env.CLI_HOME_PATH = cliConfig.cliHome;
+}
+
+function checkInputArgs() {
+  const minimist = require('minimist');
+  args = minimist(process.argv.slice(2));
+  checkArgs();
+}
+
+function checkArgs() {
+  if (args.debug) {
+    process.env.LOG_LEVEL = 'verbose';
+  } else {
+    process.env.LOG_LEVEL = 'info';
+  }
+  log.level = process.env.LOG_LEVEL;
+}
+
 function checkUserHome() {
-  console.log(userHome);
   if (!userHome || !pathExists(userHome)) {
     throw new Error(colors.red(`当前登录用户主目录不存在！`));
   }
