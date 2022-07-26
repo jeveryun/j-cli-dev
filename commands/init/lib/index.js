a14854a5a66c8ca48daddc3e57863148b5b827c0
jeveryun <jeveryun@qq.com>
2022-07-26 15:39:04 +0800

    6.9 spawn

--- commands/init/lib/index.js
+++ commands/init/lib/index.js
@@ -1,8 +1,20 @@
 'use strict';
 
 const Command = require('@j-cli-dev/command');
+const log = require('@j-cli-dev/log');
 
-class InitCommand extends Command {}
+class InitCommand extends Command {
+  init() {
+    this.projectName = this._argv[0] || '';
+    this.force = this._opts.force;
+    log.verbose('projectName', this.projectName);
+    log.verbose('force', this.force);
+  }
+
+  exec() {
+    console.log('init 的业务逻辑');
+  }
+}
 
 function init(argv) {
   return new InitCommand(argv);
--- core/exec/lib/index.js
+++ core/exec/lib/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const cp = require('child_process');
 
 const Package = require('@j-cli-dev/package');
 const log = require('@j-cli-dev/log');
@@ -54,9 +55,44 @@ async function exec() {
   }
   const rootFile = pkg.getRootFilePath();
   if (rootFile) {
-    // 在当前进程中调用
-    require(rootFile).call(null, Array.from(arguments));
-    // 在Node 子进程中调用
+    try {
+      // 在当前进程中调用
+      // require(rootFile).call(null, Array.from(arguments));
+      // 在Node 子进程中调用
+      const args = Array.from(arguments);
+      const cmd = args[args.length - 1];
+      const o = Object.create(null);
+      Object.keys(cmd).forEach((key) => {
+        if (
+          cmd.hasOwnProperty(key) &&
+          !key.startsWith('_') &&
+          key !== 'parent'
+        ) {
+          o[key] = cmd[key];
+        }
+      });
+      args[args.length - 1] = o;
+      const code = `require('${rootFile}').call(null, ${JSON.stringify(args)})`;
+      const child = spawn('node', ['-e', code], {
+        cwd: process.cwd(),
+        stdio: 'inherit',
+      });
+      child.on('error', (e) => {
+        log.error(e.message);
+        process.exit(1);
+      });
+      child.on('exit', (e) => [log.verbose('命令执行成功:' + e)]);
+    } catch (e) {
+      log.error(e.message);
+    }
   }
 }
+
+function spawn(command, args, options) {
+  const win32 = process.platform === 'win32';
+  const cmd = win32 ? 'cmd' : command;
+  const cmdArgs = win32 ? ['/c'].concat(command, args) : args;
+  return cp.spawn(cmd, cmdArgs, options || {});
+}
+
 module.exports = exec;
